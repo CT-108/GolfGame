38586d4b0e950f8113846230a19188ea3b3e804a
Iris Pecquet <iris.pecquet@lacatholille.fr>
2022-02-17 22:53:27 +0100

    Jsp trop

--- GolfProject/Assets/Scripts/IrisBallPC.cs
+++ GolfProject/Assets/Scripts/IrisBallPC.cs
@@ -14,18 +14,24 @@ public class IrisBallPC : MonoBehaviour
     private Rigidbody2D rb;
     public LineRenderer lr;
     private SpriteRenderer sr;
-    public List<GameObject> blocRoom; // empêchent au joueur d'aller dans un autre niveau tant qu'il n'a pas gagné
+    public int[] limitHits;
+    private int _currentLimitHit; // empêchent au joueur d'aller dans un autre niveau tant qu'il n'a pas gagné
     private int numberHit; // nbre de coups
     private int recoltedCoins = 0;
-    [SerializeField]
-    private TMP_Text textCoins;
+   
+    public TMP_Text textCoins;
+    public TMP_Text textHits;
+
     private bool isBeingHeld = false;
-    private bool isIn = false; //est DANS le trou
+    private bool isAbleToShoot = true;
+    private bool canMove = false; //est DANS le trou
     private int holeTime = 0; // quand la balle rebondie dans le trou de victoire
     // pour savoir dans quelle partie du niveau on est
     private bool inRoom1 = true;
     private bool inRoom2 = false;
     private bool inRoom3 = false;
+
+    private bool camIsMoving = false;
 
     public List<PhysicsMaterial2D> groundEffect; // 0 = basic 1 = sable
 
@@ -41,8 +47,10 @@ public class IrisBallPC : MonoBehaviour
 
     private void Start()
     {
-        numberHit = 0; 
+        _currentLimitHit = 0;
+        numberHit = 0;
         textCoins.text = "" + recoltedCoins;
+        textHits.text = "" + numberHit;
 
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
@@ -50,25 +58,34 @@ public class IrisBallPC : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (rb.velocity.magnitude != 0)
+            isAbleToShoot = false;
+        else
+            isAbleToShoot = true;
+
+        //(isAbleToShoot);
+
+        if (Input.GetMouseButtonDown(0) && isAbleToShoot == true && !camIsMoving)
         {
+
             isBeingHeld = true;
 
             if (Input.GetMouseButtonDown(0))
             {
                 DragStart();
             }
-            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-            {
-                Dragging();
-            }
+        }
+
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            Dragging();
         }
 
         if (Input.GetMouseButtonUp(0) && isBeingHeld == true)
         {
             DragRealease();
             numberHit++; // on compte un coup quand le joueur lâche la souris/doigt
-            Debug.Log(numberHit);
+            //(numberHit);
             isBeingHeld = false;
         }
 
@@ -86,10 +103,13 @@ public class IrisBallPC : MonoBehaviour
     }
     private void Dragging()
     {
-        Vector2 draggingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //dragStartPos.z = 0f;
-        lr.positionCount = 2;
-        lr.SetPosition(1, draggingPos);
+        if (isBeingHeld == true)
+        {
+            Vector2 draggingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //dragStartPos.z = 0f;
+            lr.positionCount = 2;
+            lr.SetPosition(1, draggingPos);
+        }
     }
     private void DragRealease()
     {
@@ -105,43 +125,26 @@ public class IrisBallPC : MonoBehaviour
 
     private void CamMouvement()
     {
-        if (isIn) // qd la balle est dans le trou la caméra slide vers le prochain niveau
+        if (canMove) // qd la balle est dans le trou la caméra slide vers le prochain niveau
         {
             float distCovered = (Time.time - startTime) * speed;
             float fractionOfJourney = distCovered / journeyLength;
             cam.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+            camIsMoving = true;
         }
     }
-    
+
 
     private void HitLimit() // limite des coups, pas encore setup le "perdu" etc, ça reset la room dans laquelle on est
     {
-        if(inRoom1)
-        {
-            if (numberHit >= 5) // nombre modulable
-            {
-                Scene scene = SceneManager.GetActiveScene();
-                SceneManager.LoadScene(scene.name);
-                Debug.Log("Reload Scene"); // on restart la scene dans laquelle ON EST
-            }
-        } 
         if (inRoom2)
-        {
-            if (numberHit >= 6)
-            {
-                Scene scene = SceneManager.GetActiveScene();
-                SceneManager.LoadScene(scene.name);
-                Debug.Log("Reload Scene");
-            }
-        }
+            _currentLimitHit = 1;
         if (inRoom3)
+            _currentLimitHit = 2;
+
+        if (numberHit >= limitHits[(_currentLimitHit)])
         {
-            if (numberHit >= 10)
-            {
-                Scene scene = SceneManager.GetActiveScene();
-                SceneManager.LoadScene(scene.name);
-                Debug.Log("Reload Scene");
-            }
+            StartCoroutine(Perdu());
         }
     }
 
@@ -149,32 +152,30 @@ public class IrisBallPC : MonoBehaviour
     {
         if (collision.gameObject.tag == "Hole") // quand la balle arrive dans le trou 
         {
-            isIn = true; // elle est bien dedans
-            StartCoroutine(isInChrono()); // on démarre un chrono qui sert à la faire disparaître (pas encore setup)
+            canMove = true; // elle est bien dedans
+            StartCoroutine(CanMoveChrono()); // on démarre un chrono qui sert à la faire disparaître (pas encore setup)
             holeTime++; // la variable passe donc à 1
             if (holeTime == 1) // on l'empêche de comptabiliser les rebonds de la balle dans le trou comme des victoires à chaque fois
-            { 
-                Debug.Log("Victoire");
+            {
                 // pour calculer la distance que la cam aura à faire à l'instant T
                 startTime = Time.time;
                 journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
 
                 sr.DOFade(0, 1.5f); // on fait un fondu pour faire disparaître la balle
-                StartCoroutine(fadeIn()); // on lance une coroutine pour remettre l'alpha à 0                
+                StartCoroutine(FadeIn()); // on lance une coroutine pour remettre l'alpha à 0
             }
+
             
-            blocRoom[0].SetActive(false); // on désactive l'obstacle entre les 2 niveaux concernés
 
         }
         if (collision.gameObject.tag == "Hole2")
         {
             holeTime = 0;
-            isIn = true;
-            StartCoroutine(isInChrono());
+            canMove = true;
+            StartCoroutine(CanMoveChrono());
             holeTime++;
             if (holeTime == 1)
             {
-                Debug.Log("Victoire");
                 //cam 
                 startMarker.position = new Vector3(22.21f, 0, -20);
                 endMarker.position = new Vector3(44.42f, 0, -20);
@@ -182,38 +183,38 @@ public class IrisBallPC : MonoBehaviour
                 journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
 
                 sr.DOFade(0, 1.5f);
-                StartCoroutine(fadeIn());
+                StartCoroutine(FadeIn());
             }
-            blocRoom[1].SetActive(false);
+            
 
         }
 
         if (collision.gameObject.tag == "Hole3")
         {
-            Debug.Log("Victoire");
-            //StartCoroutine(WaitForNextScene()); 
+            holeTime = 0;
+            canMove = true;
         }
 
 
         //Quand la balle arrive dans la room d'après elle ne peut plus revenir en arrière
         if (collision.gameObject.tag == "Room2")
         {
+            
             inRoom2 = true;
             inRoom1 = false;
             inRoom3 = false;
-            
-            blocRoom[0].SetActive(true);
+       
             numberHit = 0;
-            //rb.sharedMaterial = test;
+    
         }
 
         if (collision.gameObject.tag == "Room3")
         {
+            
             inRoom2 = false;
             inRoom1 = false;
             inRoom3 = true;
-
-            blocRoom[1].SetActive(true);
+           
             numberHit = 0;
         }
 
@@ -247,20 +248,28 @@ public class IrisBallPC : MonoBehaviour
         SceneManager.LoadScene("Scene_Iris 1");
     }*/
 
-    IEnumerator isInChrono() 
+    IEnumerator CanMoveChrono()
     {
-        yield return new WaitForSeconds(5);
-        isIn = false;
+        yield return new WaitForSeconds(3);
+        canMove = false;
+        camIsMoving = false;
     }
 
-    IEnumerator fadeIn()
+    IEnumerator FadeIn()
     {
         yield return new WaitForSeconds(2.3f); //le temps que la cam soit arrivée au level suivant
-        if(inRoom1)
+        if (inRoom1)
             ball.transform.position = new Vector3(12.31f, -4.33f, 0);
-        if(inRoom2)
+        if (inRoom2)
             ball.transform.position = new Vector3(34.34f, -4.33f, 0);
-        Debug.Log("FADE");
         sr.DOFade(1, 1.5f); // on reset l'alpha de la balle à 1
+    }
+
+    IEnumerator Perdu()
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("t'es nul");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
