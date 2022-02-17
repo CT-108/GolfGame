798527f2a2114a113210714dcfbe7800461fa179
Iris Pecquet <iris.pecquet@lacatholille.fr>
2022-02-17 18:54:37 +0100

    cam+chgmnt ground+coups limités

--- GolfProject/Assets/Scripts/IrisBallPC.cs
+++ GolfProject/Assets/Scripts/IrisBallPC.cs
@@ -10,39 +10,34 @@ public class IrisBallPC : MonoBehaviour
     public float maxDrag = 5f;
     public Rigidbody2D rb;
     public LineRenderer lr;
-    public List<GameObject> blocRoom;
-    private int numberHit;
+    public List<GameObject> blocRoom; // empêchent au joueur d'aller dans un autre niveau tant qu'il n'a pas gagné
+    private int numberHit; // nbre de coups
     private int recoltedCoins = 0;
     [SerializeField]
     private TMP_Text textCoins;
     private bool isBeingHeld = false;
-    public bool isIn = false;
+    private bool isIn = false; //est DANS le trou
+    private int holeTime = 0; // quand la balle rebondie dans le trou de victoire
+    // pour savoir dans quelle partie du niveau on est
+    private bool inRoom1 = true;
+    private bool inRoom2 = false;
+    private bool inRoom3 = false;
 
-    //public PhysicsMaterial2D basic;
-    public PhysicsMaterial2D test;
+    public List<PhysicsMaterial2D> groundEffect; // 0 = basic 1 = sable
 
     Vector2 dragStartPos;
-    Touch touch;
 
-    public static IrisBallPC Instance;
-
+    //mouvement de cam quand on gagne le niveau, elle décale d'un point à l'autre
     public GameObject cam;
-
     public Transform startMarker;
     public Transform endMarker;
-
     public float speed;
     private float startTime;
     private float journeyLength;
 
-    private void Awake()
-    {
-        Instance = this;
-    }
-
     private void Start()
     {
-        numberHit = 0;
+        numberHit = 0; 
         textCoins.text = "" + recoltedCoins;
 
         rb = GetComponent<Rigidbody2D>();
@@ -67,18 +62,16 @@ public class IrisBallPC : MonoBehaviour
         if (Input.GetMouseButtonUp(0) && isBeingHeld == true)
         {
             DragRealease();
-            numberHit++;
+            numberHit++; // on compte un coup quand le joueur lâche la souris/doigt
             Debug.Log(numberHit);
             isBeingHeld = false;
         }
 
-        if (isIn)
-        {            
-            float distCovered = (Time.time - startTime) * speed;
-            float fractionOfJourney = distCovered / journeyLength;
-            cam.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
-        }
+        CamMouvement();
+        //HitLimit();
     }
+
+
     private void DragStart()
     {
         dragStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -105,42 +98,109 @@ public class IrisBallPC : MonoBehaviour
         rb.AddForce(clampedForce, ForceMode2D.Impulse);
     }
 
+    private void CamMouvement()
+    {
+        if (isIn) // qd la balle est dans le trou la caméra slide vers le prochain niveau
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            cam.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+        }
+    }
+    
+
+    private void HitLimit() // limite des coups, pas encore setup le "perdu" etc, ça reset la room dans laquelle on est
+    {
+        if(inRoom1)
+        {
+            if (numberHit >= 5) // nombre modulable
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                Debug.Log("Reload Scene"); // on restart la scene dans laquelle ON EST
+            }
+        } 
+        if (inRoom2)
+        {
+            if (numberHit >= 6)
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                Debug.Log("Reload Scene");
+            }
+        }
+        if (inRoom3)
+        {
+            if (numberHit >= 10)
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                Debug.Log("Reload Scene");
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Quand la balle atteint le trou et peut passer à la room suivante
-        if (collision.gameObject.tag == "Hole")
+        if (collision.gameObject.tag == "Hole") // quand la balle arrive dans le trou 
         {
-            Debug.Log("Victoire");
-            blocRoom[0].SetActive(false);
-            isIn = true;
-            // Keep a note of the time the movement started.
+            isIn = true; // elle est bien dedans
+            StartCoroutine(isInChrono()); // on démarre un chrono qui sert à la faire disparaître (pas encore setup)
+            holeTime++; // la variable passe donc à 1
+            if (holeTime == 1) // on l'empêche de comptabiliser les rebonds de la balle dans le trou comme des victoires à chaque fois
+            { 
+                Debug.Log("Victoire");
+                // pour calculer la distance que la cam aura à faire à l'instant T
+                startTime = Time.time;
+                journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+            }
+            blocRoom[0].SetActive(false); // on désactive l'obstacle entre les 2 niveaux concernés
 
-            startTime = Time.time;
-            // Calculate the journey length.
-            journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
         }
         if (collision.gameObject.tag == "Hole2")
         {
-            Debug.Log("Victoire");
+            holeTime = 0;
+            isIn = true;
+            StartCoroutine(isInChrono());
+            holeTime++;
+            if (holeTime == 1)
+            {
+                Debug.Log("Victoire");
+                //cam 
+                startMarker.position = new Vector3(22.21f, 0, -20);
+                endMarker.position = new Vector3(44.42f, 0, -20);
+                startTime = Time.time;
+                journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+            }
             blocRoom[1].SetActive(false);
+
         }
+
         if (collision.gameObject.tag == "Hole3")
         {
             Debug.Log("Victoire");
-            StartCoroutine(WaitForNextScene());
+            //StartCoroutine(WaitForNextScene()); 
         }
 
 
         //Quand la balle arrive dans la room d'après elle ne peut plus revenir en arrière
         if (collision.gameObject.tag == "Room2")
         {
+            inRoom2 = true;
+            inRoom1 = false;
+            inRoom3 = false;
+            
             blocRoom[0].SetActive(true);
             numberHit = 0;
-            rb.sharedMaterial = test;
+            //rb.sharedMaterial = test;
         }
 
         if (collision.gameObject.tag == "Room3")
         {
+            inRoom2 = false;
+            inRoom1 = false;
+            inRoom3 = true;
+
             blocRoom[1].SetActive(true);
             numberHit = 0;
         }
@@ -154,11 +214,30 @@ public class IrisBallPC : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
+        if (collision.gameObject.tag == "Sand")
+        {
+            rb.sharedMaterial = groundEffect[1];
+            // à la base j'avais voulu faire une collision entre la balle et le sable directement mais il y avait quand même un rebond qui se faisait avec l'ancien material
+            // du coup j'ai créé un enfant à chaque bloc de sable qui a juste un collider plus épais que l'original comme ça la balle détecte ce collider là
+            // ce qui fait qu'elle a le temps de changer de material avant de toucher le vrai sol qui modifie son material
+        }
+
+        if (collision.gameObject.tag == "BasicGround")
+        {
+            rb.sharedMaterial = groundEffect[0];
+            // idem qu'au dessus
+        }
     }
 
-    IEnumerator WaitForNextScene()
+    /*IEnumerator WaitForNextScene()
     {
         yield return new WaitForSeconds(2);
         SceneManager.LoadScene("Scene_Iris 1");
+    }*/
+
+    IEnumerator isInChrono() 
+    {
+        yield return new WaitForSeconds(5);
+        isIn = false;
     }
 }
