02bdd8b67870ab0605f2e28db8615de1f52ceb00
CT-108 <jer59billemont@gmail.com>
2022-02-24 11:08:52 +0100

    Menu Bien Update Bien Fonctionnel
    SIUUUUUUUUUUUUUUUUUUUUU

--- GolfProject/Assets/Scripts/MenusScript.cs
+++ GolfProject/Assets/Scripts/MenusScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class MenusScript : MonoBehaviour
 {
@@ -10,14 +11,16 @@ public class MenusScript : MonoBehaviour
     public SpriteRenderer Fader;
 
     public int FadeInTime;
-
     public int FadeOutTime;
 
+    public GameObject MainCanvas;
+    public GameObject OptionsCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(FadeOut());
+        OptionsCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,17 +31,22 @@ public class MenusScript : MonoBehaviour
 
     public void OnClickPlay()
     {
-
+        StartCoroutine(FadeInAndPlay());
     }
 
     public void OnClickOptions()
     {
-
+        StartCoroutine(FadeInOptions());
     }
 
     public void OnClickExit()
     {
+        Application.Quit();
+    }
 
+    public void OnClickBack()
+    {
+        StartCoroutine(FadeBackInMenu());
     }
 
     IEnumerator FadeIn()
@@ -51,5 +59,32 @@ public class MenusScript : MonoBehaviour
     {
         Fader.DOFade(0, FadeOutTime);
         yield return new WaitForSeconds(FadeOutTime);
+    }
+
+    IEnumerator FadeInAndPlay()
+    {
+        Fader.DOFade(1, FadeInTime);
+        yield return new WaitForSeconds(FadeInTime);
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator FadeInOptions()
+    {
+        Fader.DOFade(1, FadeInTime);
+        yield return new WaitForSeconds(FadeInTime);
+        MainCanvas.SetActive(false);
+        OptionsCanvas.SetActive(true);
+        Fader.DOFade(0, FadeInTime);
+        yield return new WaitForSeconds(FadeInTime);
+    }
+
+    IEnumerator FadeBackInMenu()
+    {
+        Fader.DOFade(1, FadeInTime);
+        yield return new WaitForSeconds(FadeInTime);
+        MainCanvas.SetActive(true);
+        OptionsCanvas.SetActive(false);
+        Fader.DOFade(0, FadeInTime);
+        yield return new WaitForSeconds(FadeInTime);
     }
 }
